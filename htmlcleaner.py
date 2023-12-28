9e2407a076c0a1a3f1aa2df9c3a74073bcc5d251
Vincent Wehren <vinweh@users.noreply.github.com>
2023-12-27 18:58:07 -0800

    Support <main> as an authoratative tag for main content

--- htmlcleaner.py
+++ htmlcleaner.py
@@ -1,8 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+removables = ["nav", 
+                      "header", 
+                      "footer", 
+                      "head", 
+                      "*[style='display:none']",
+                      "*[style='display: none;']",      
+                      "*[role='navigation']"
+                      ,"*[aria-hidden='true']"]
+
 class HTMLCleaner:
     """Class to clean HTML of elements that do not contain 'main content'"""
+    
     @staticmethod
     def remove_all_nav(soup, removables=[]):
         """Remove known nav, header, footer, and other nav stuff from soup"""
@@ -12,22 +22,44 @@ class HTMLCleaner:
                 continue
             elem.decompose()
         return soup
+    
+    @staticmethod
+    def main_content(soup):
+        """Get main content from soup"""
+        
+        mains = soup.select("main") #get all main elements, tehre should only be one non hidden one
+        mains_non_hidden = [m for m in mains if not "hidden" in m.attrs]
+        if mains_non_hidden: # if there is a non hidden main element, use that
+            main = mains_non_hidden[0] # get first non hidden main element
+            if main: # if there is a viable main tag, remove all nav elements within it (if ny)
+                s = HTMLCleaner.remove_all_nav(main, removables)
+                return(" ".join(" ".join(t.strip().split()) \
+                            for t in s.stripped_strings if t.strip() != ""))
+        else:
+            return ""
 
     @staticmethod
     def stripped(content, custom_removables=[]):
-        removables = ["nav", 
-                      "header", 
-                      "footer", 
-                      "head", 
-                      "*[style='display:none']",
-                      "*[style='display: none;']",  
-                      "*[role='navigation']"
-                      ,"*[aria-hidden='true']"]
         removables.extend(custom_removables)
         soup = BeautifulSoup(content, 'lxml')
         HTMLCleaner.remove_all_nav(soup, removables)
         return(" ".join(" ".join(t.strip().split()) \
                         for t in soup.stripped_strings if t.strip() != ""))
+    
+    def clean_text(content, custom_removables=[]):
+        """Get clean text from HTML content"""
+        soup = BeautifulSoup(content, 'lxml')
+        text = ""
+        main = HTMLCleaner.main_content(soup) # get main content as specified by main tag
+        print(f"Main text length: {len(main)}")
+        if main: 
+            text = main
+        else:
+            text = HTMLCleaner.stripped(content) # get stripped text, irrespective of main tag
+            print(f"Stripped text length: {len(text)}")
+        print(f"Clean text length in bytes: {len(text.encode('utf-8'))}")
+        return text
+        
         
 
 if __name__ == "__main__":
@@ -41,11 +73,24 @@ if __name__ == "__main__":
     #custom_removables = ['div.reflist', '.vector-dropdown-content', '.uls-lcd-region-section']
     #url = "https://stackoverflow.com/questions/3379166/writing-blob-from-sqlite-to-file-using-python"
     #custom_removables = ['#answers-header', '#sidebar', '#post-form', '.bottom-notice']
-    url = "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9069952433271538175%22%7D&n_type=1&p_from=3"
+    #url = "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9069952433271538175%22%7D&n_type=1&p_from=3"
+    url = "https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/migration?tabs=python%2Cdalle-fix"
     html = requests.get(url).text
+    
     print(f"HTML length: {len(html)}")
+    
     soup = BeautifulSoup(html, 'lxml')
-    text = HTMLCleaner.stripped(html)
+    main = HTMLCleaner.main_content(soup) # get main content as specified by main tag
+    print(f"Main text length: {len(main)}")
+    stripped = HTMLCleaner.stripped(html) # get stripped text, irrespective of main tag
+    text  = ""
+    
+    if main:
+        text = main
+    else:
+        text = stripped
+    
+    print(f"Stripped text length: {len(stripped)}")
     print(f"Clean text length: {len(text)}")
     print(f"Clean text length in bytes: {len(text.encode('utf-8'))}")
     with open("stripped.txt", "w", encoding='utf-8') as f:
